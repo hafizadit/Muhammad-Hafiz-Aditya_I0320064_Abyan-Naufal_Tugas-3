d769428fe5eb6b6da741f030fc7fb718fdabb1b0
hafizadit <mha.hafiz21@gmail.com>
2021-03-19 08:57:00 +0700

    Memperbaiki struktur

--- I0320064_soal2_tugas3.py
+++ I0320064_soal2_tugas3.py
@@ -1,19 +1,19 @@
 # Membuat dictionary yang berisikan nama, hobi (lebih dari dua), sosmed (lebih dari dua), makanan favorit (lebih dari dua)
 mydict = {"Nama":"Muhammad Hafiz Aditya",
-"Hobi":("Fotografi","Membaca buku","Videografi"),
-"Sosmed":("Instagram : @hafiz_adit","Whatsapp : 081393856662","Line : tatank322"),
-"Makanan Favorit":("Bakso","Sate","Ayam")}
+"Hobi":["Fotografi","Membaca buku","Videografi"],
+"Sosmed":["Instagram : @hafiz_adit","Whatsapp : 081393856662","Line : tatank322"],
+"Makanan Favorit":["Bakso","Sate","Ayam"]}
 print("\nDictionary sebelum diubah :\n", mydict)
 
 #Meengubah satu hobi dan sosial media
-mydict["Hobi"] = ("Fotografi", "Membaca buku", "Mendengarkan Musik")
-mydict["Sosmed"] = ("Instagram : @hafiz_adit","Whatsapp : 081393856662","LinkedIn : Muhammad Hafiz Aditya")
+mydict["Hobi"][2] = ("Mendengarkan Musik")
+mydict["Sosmed"][2] = ("LinkedIn : Muhammad Hafiz Aditya")
 
 # Menghapus dua makanan favorit
-mydict["Makanan Favorit"] = ("Sate")
+del mydict["Makanan Favorit"][0:2]
 
 #Menambahkan satu hobi
-mydict["Hobi"] = ("Fotografi", "Membaca buku", "Mendengarkan Musik","Tidur")
+mydict["Hobi"].append("Tidur")
 
 #Menampilkan dictionary
 print("\nDictionary setelah diubah menjadi :\n", mydict,"\n")
